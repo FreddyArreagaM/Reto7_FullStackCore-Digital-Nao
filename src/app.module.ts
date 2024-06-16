1070af5cfa326693a3923270b715bf42e9d7092f
Freddy Arreaga Morán <127709400+FreddyArreagaM@users.noreply.github.com>
2024-06-15 21:47:37 -0500

    feat: add enviroments db

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,30 +7,41 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthExceptionFilter } from './auth/jwt/jwt-auth-exception.filter';
 import { APP_FILTER } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as path from 'path';
+
+// Carga las variables de entorno desde el archivo .env
+const envFilePath = path.resolve(__dirname, '..', '.env');
+const envFileExists = fs.existsSync(envFilePath);
+if (envFileExists) {
+  dotenv.config({ path: envFilePath });
+} else {
+  console.error('.env file not found, using default environment variables.');
+}
 
 @Module({
   imports: [
-    //Declaración de variables para conexión con MySQL
-    TypeOrmModule.forRoot(
-      {
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Admin12345@@',
-      database: 'books_db',
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: ['dist/**/*.entity.js'],
       synchronize: true,
-      }),
+    }),
     BooksModule,
-    AuthModule],
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [
     {
       provide: APP_FILTER,
       useClass: JwtAuthExceptionFilter,
     },
-    AppService
+    AppService,
   ],
 })
 export class AppModule {}
